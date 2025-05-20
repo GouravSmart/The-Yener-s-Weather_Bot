04e9645594d524a7fb52ec1025442dbdebc80725
GouravSmart <gouravjaiswal845845@gmail.com>
2025-05-20 16:23:09 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -73,6 +73,7 @@ const [weather, setWeather] = useState<WeatherData | null>(null);
             className="bg-blue-600 px-4 py-2 rounded-md hover:bg-blue-700 disabled:opacity-50"
             // disabled={loading}
           >
+            Search 
             {/* {loading ? "Loading..." : "Get Forecast"} */}
           </button>
         </div>
